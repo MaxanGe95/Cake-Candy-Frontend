4309833c44e3c77e63c943355151b324395f8054
Sibel Genc <sibelgencasist@gmail.com>
2025-02-24 13:30:33 +0100

    Endprodukte korrigiert

--- cake-and-candy/src/pages/Products.jsx
+++ cake-and-candy/src/pages/Products.jsx
@@ -1,25 +1,6 @@
-import products from "../products"
-import Product from "../components/Product"
 
-function Products() {
-    return (
-      <div>
-        <h1>Willkommen bei Produkte</h1>
-        <div className="flex justify-center items-center min-h-screen ">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {products.map((product, index) => (
-              <Product
-                key={index}
-                image={product.image}
-                title={product.title}
-                description={product.description}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-    );
+  function Products() {
+    return <h1>Willkommen bei EndProducts</h1>;
   }
   
   export default Products;
-  
