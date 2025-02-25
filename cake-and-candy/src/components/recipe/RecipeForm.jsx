8ed785a76c953308f074fee326830ab4941ca7ed
Sibel Genc <sibelgencasist@gmail.com>
2025-02-25 21:16:16 +0100

    Darstellung Rezeptseite verbessert

--- cake-and-candy/src/components/recipe/RecipeForm.jsx
+++ cake-and-candy/src/components/recipe/RecipeForm.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { FaTrash } from "react-icons/fa";
+import { IconChevronDown } from "@tabler/icons-react";
 
-const ingredientsList = ['Mehl', 'Zucker', 'Eier', 'Milch', 'Butter'];
-const unitsList = ['g', 'kg', 'ml', 'l', 'Stück'];
+const ingredientsList = ["Mehl", "Zucker", "Eier", "Milch", "Butter"];
+const unitsList = ["g", "kg", "ml", "l", "Stück"];
 
 const RecipeForm = ({ recipe, onSave, onCancel }) => {
   const [newRecipe, setNewRecipe] = useState(recipe);
@@ -10,12 +12,17 @@ const RecipeForm = ({ recipe, onSave, onCancel }) => {
   const addIngredient = () => {
     setNewRecipe({
       ...newRecipe,
-      ingredients: [...newRecipe.ingredients, { name: '', amount: 0, unit: '' }]
+      ingredients: [
+        ...newRecipe.ingredients,
+        { name: "", amount: 0, unit: "" },
+      ],
     });
   };
 
   const removeIngredient = (index) => {
-    const updatedIngredients = newRecipe.ingredients.filter((_, i) => i !== index);
+    const updatedIngredients = newRecipe.ingredients.filter(
+      (_, i) => i !== index
+    );
     setNewRecipe({ ...newRecipe, ingredients: updatedIngredients });
   };
 
@@ -35,7 +42,7 @@ const RecipeForm = ({ recipe, onSave, onCancel }) => {
   const validateField = (field, value) => {
     setErrors((prevErrors) => ({
       ...prevErrors,
-      [field]: !value
+      [field]: !value,
     }));
   };
 
@@ -55,7 +62,7 @@ const RecipeForm = ({ recipe, onSave, onCancel }) => {
 
   const saveRecipe = () => {
     if (!validateRecipe()) {
-      alert('Bitte füllen Sie alle Pflichtfelder aus.');
+      alert("Bitte füllen Sie alle Pflichtfelder aus.");
       return;
     }
     onSave(newRecipe);
@@ -63,65 +70,105 @@ const RecipeForm = ({ recipe, onSave, onCancel }) => {
 
   return (
     <div className="mb-4">
-      <h2 className="text-xl font-semibold text-teal-300">{recipe.id ? 'Rezept bearbeiten' : 'Neues Rezept'}</h2>
+      <h2 className="text-xl font-semibold text-teal-300">
+        {recipe.id ? "Rezept bearbeiten" : "Neues Rezept"}
+      </h2>
       <input
         type="text"
         placeholder="Rezeptname"
         value={newRecipe.name}
-        onChange={(e) => handleRecipeChange('name', e.target.value)}
-        className={`border p-2 mb-2 w-full ${errors.name ? 'border-red-500' : ''}`}
+        onChange={(e) => handleRecipeChange("name", e.target.value)}
+        className={`border p-2 mb-2 w-full ${
+          errors.name ? "border-red-500" : ""
+        }`}
       />
       <textarea
         placeholder="Beschreibung"
         value={newRecipe.description}
-        onChange={(e) => handleRecipeChange('description', e.target.value)}
-        className={`border p-2 mb-2 w-full resize-none ${errors.description ? 'border-red-500' : ''}`}
-        rows={Math.max(3, newRecipe.description.split('\n').length)}
+        onChange={(e) => handleRecipeChange("description", e.target.value)}
+        className={`border p-2 mb-2 w-full resize-none ${
+          errors.description ? "border-red-500" : ""
+        }`}
+        rows={Math.max(3, newRecipe.description.split("\n").length)}
       />
       <h3 className="text-lg font-semibold text-teal-400">Zutaten</h3>
       {newRecipe.ingredients.map((ingredient, index) => (
-        <div key={index} className="flex mb-2 items-center">
-          <select
-            value={ingredient.name}
-            onChange={(e) => handleIngredientChange(index, 'name', e.target.value)}
-            className={`border p-2 mr-2 w-1/4 ${errors[`ingredient${index}name`] ? 'border-red-500' : ''}`}
-          >
-            <option value="">Zutat wählen</option>
-            {ingredientsList.map((ingredientName) => (
-              <option key={ingredientName} value={ingredientName}>
-                {ingredientName}
-              </option>
-            ))}
-          </select>
+        <div key={index} className="flex mb-2 items-center -mx-1">
+          <div className="relative w-2/4 mx-1">
+            <select
+              value={ingredient.name}
+              onChange={(e) =>
+                handleIngredientChange(index, "name", e.target.value)
+              }
+              className={`block appearance-none w-full border px-4 py-2 pr-8 rounded shadow leading-tight h-10 focus:outline-none focus:shadow-outline ${
+                errors[`ingredient${index}name`] ? "border-red-500" : ""
+              }`}
+            >
+              <option value="">Zutat wählen</option>
+              {ingredientsList.map((ingredientName) => (
+                <option key={ingredientName} value={ingredientName}>
+                  {ingredientName}
+                </option>
+              ))}
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
+              <IconChevronDown className="w-4 h-4" />
+            </div>
+          </div>
           <input
             type="number"
             placeholder="Menge"
             value={ingredient.amount}
-            onChange={(e) => handleIngredientChange(index, 'amount', e.target.value)}
-            className={`border p-2 w-1/4 ${errors[`ingredient${index}amount`] ? 'border-red-500' : ''}`}
+            onChange={(e) =>
+              handleIngredientChange(index, "amount", e.target.value)
+            }
+            className={`border p-2 m-1 w-1/4 h-10 rounded ${
+              errors[`ingredient${index}amount`] ? "border-red-500" : ""
+            }`}
           />
-          <select
-            value={ingredient.unit}
-            onChange={(e) => handleIngredientChange(index, 'unit', e.target.value)}
-            className={`border p-2 w-1/4 ${errors[`ingredient${index}unit`] ? 'border-red-500' : ''}`}
+          <div className="relative w-1/4 m-1">
+            <select
+              value={ingredient.unit}
+              onChange={(e) =>
+                handleIngredientChange(index, "unit", e.target.value)
+              }
+              className={`block appearance-none w-full border px-4 py-2 pr-8 rounded shadow leading-tight h-10 focus:outline-none focus:shadow-outline ${
+                errors[`ingredient${index}unit`] ? "border-red-500" : ""
+              }`}
+            >
+              <option value="">Einheit wählen</option>
+              {unitsList.map((unit) => (
+                <option key={unit} value={unit}>
+                  {unit}
+                </option>
+              ))}
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
+              <IconChevronDown className="w-4 h-4" />
+            </div>
+          </div>
+          <button
+            onClick={() => removeIngredient(index)}
+            className="bg-red-500 text-white m-1 p-2 cursor-pointer h-10 w-10 transition ease-in-out hover:scale-110 "
           >
-            <option value="">Einheit wählen</option>
-            {unitsList.map((unit) => (
-              <option key={unit} value={unit}>
-                {unit}
-              </option>
-            ))}
-          </select>
-          <button onClick={() => removeIngredient(index)} className="bg-red-500 text-white p-2 ml-2">Löschen</button>
+            <FaTrash className="m-auto" />
+          </button>
         </div>
       ))}
-      <button onClick={addIngredient} className="bg-teal-500 text-white p-2 mb-2">Zutat hinzufügen</button>
+      <button
+        onClick={addIngredient}
+        className="bg-teal-500 text-white p-2 mb-2"
+      >
+        Zutat hinzufügen
+      </button>
       <h3 className="text-lg font-semibold text-teal-400">Hilfsmittel</h3>
       <input
         type="text"
         placeholder="Hilfsmittel"
-        value={newRecipe.tools.join(', ')}
-        onChange={(e) => handleRecipeChange('tools', e.target.value.split(', '))}
+        value={newRecipe.tools.join(", ")}
+        onChange={(e) =>
+          handleRecipeChange("tools", e.target.value.split(", "))
+        }
         className="border p-2 mb-2 w-full"
       />
       <h3 className="text-lg font-semibold text-teal-400">Gesamtmenge</h3>
@@ -129,11 +176,17 @@ const RecipeForm = ({ recipe, onSave, onCancel }) => {
         type="number"
         placeholder="Gesamtmenge"
         value={newRecipe.totalAmount}
-        onChange={(e) => handleRecipeChange('totalAmount', e.target.value)}
-        className={`border p-2 mb-2 w-full ${errors.totalAmount ? 'border-red-500' : ''}`}
+        onChange={(e) => handleRecipeChange("totalAmount", e.target.value)}
+        className={`border p-2 mb-2 w-full ${
+          errors.totalAmount ? "border-red-500" : ""
+        }`}
       />
-      <button onClick={saveRecipe} className="bg-teal-600 text-white p-2">{recipe.id ? 'Rezept aktualisieren' : 'Rezept speichern'}</button>
-      <button onClick={onCancel} className="bg-gray-500 text-white p-2 ml-2">Abbrechen</button>
+      <button onClick={saveRecipe} className="bg-teal-600 text-white p-2">
+        {recipe.id ? "Rezept aktualisieren" : "Rezept speichern"}
+      </button>
+      <button onClick={onCancel} className="bg-gray-500 text-white p-2 ml-2">
+        Abbrechen
+      </button>
     </div>
   );
 };
--- cake-and-candy/src/components/recipe/RecipeList.jsx
+++ cake-and-candy/src/components/recipe/RecipeList.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React from "react";
+import { FaTrash } from "react-icons/fa";
 
 const RecipeList = ({ recipes, onSelect, onDelete }) => {
   return (
     <div>
-      <h2 className="text-xl font-semibold text-teal-500">Gespeicherte Rezepte</h2>
+      <h2 className="text-xl font-semibold text-teal-500">
+        Gespeicherte Rezepte
+      </h2>
       <ul>
         {recipes.map((recipe, index) => (
-          <li key={index} className="border-t py-2 flex justify-between items-center">
+          <li
+            key={index}
+            className="border-t py-2 flex justify-between items-center"
+          >
             <span className="cursor-pointer" onClick={() => onSelect(recipe)}>
               {recipe.name}
             </span>
-            <button onClick={() => onDelete(recipe.id)} className="bg-red-500 text-white p-2 ml-2">Löschen</button>
+            <button
+              onClick={() => onDelete(recipe.id)}
+              className="bg-red-500 text-white p-2 ml-2 cursor-pointer transition ease-in-out hover:scale-110 "
+            >
+              <FaTrash />
+            </button>
           </li>
         ))}
       </ul>
