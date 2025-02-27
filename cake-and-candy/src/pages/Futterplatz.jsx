abf86cb07727b65ee8e1c2fb660aeaa0da67f379
nicoR <nicolas.roemershaeuser@web.de>
2025-02-27 14:01:46 +0100

    layout anpassung(textarea form und farben, border ¬ co.)

--- cake-and-candy/src/pages/Futterplatz.jsx
+++ cake-and-candy/src/pages/Futterplatz.jsx
@@ -1,22 +1,17 @@
 function Futterplatz() {
   return (
-    <div className="container mx-auto p-6">
+    <div className="container mx-auto p-6 ">
       <h1>Willkommen bei Futterplatz</h1>
       <form action="submit" name="work-experience" className="text-center">
-        <label
-          htmlFor="work-experience"
-          name="work-experience"
-          className="text-center"
-        >
+        <label htmlFor="work-experience" className="text-center text-sm">
           Inputfeld für RDP Rechnungsvordruck
         </label>
         <textarea
           name="work-experience"
           rows="10"
           cols="150"
-          className="bg-slate-300 border-2 rounded text-black p-4"
+          className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-100"
         ></textarea>
-
         <button
           type="submit"
           className="bg-teal-950 cursor-pointer border-1 rounded px-4 py-2 my-4"
@@ -25,33 +20,49 @@ function Futterplatz() {
         </button>
       </form>
       <fieldset>
-        <legend>Meine Lieblings-Programmiersprache:</legend>
         <div>
           <input type="radio" id="B2B" name="B2B" value="B2B" />
           <label htmlFor="B2B">B2B</label>
         </div>
         <div>
-          <input type="radio" id="B2C" name="language" value="B2C" />
-          <label htmlFor="javascript-language">B2C</label>
+          <input type="radio" id="B2C" name="B2C" value="B2C" />
+          <label htmlFor="B2C">B2C</label>
         </div>
       </fieldset>
 
-      <label for="welcheFirma">Firma/ Privatkunde:</label>
-        <select id="computer-os" name="computer-os">
-          <option value="ubuntu">Ubuntu</option>
-          <option value="macos">MacOS</option>
-          <option value="windows">Windows</option>
-        </select>
+      {/*       <label htmlFor="welcheFirma">Firma/ Privatkunde:</label>
+      <select id="welcheFirma" name="welcheFirma" className="bg-slate-300 border-2 rounded text-black p-2 ml-3">
+        <option value="firma">Firma</option>
+        <option value="privatkunde">Privat Kunde</option>
+      </select> */}
+
+      <label htmlFor="auswahl">Firma/ Privatkunde:</label>
+      <select
+        id="auswahl"
+        name="auswahl"
+        className="bg-slate-300 border-2 rounded text-black p-2 ml-2"
+      >
+        <optgroup label="Firma">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </optgroup>
+        <optgroup label="Privat Kunde">
+          <option value="11">11</option>
+          <option value="111">111</option>
+          <option value="1111">1111</option>
+        </optgroup>
+      </select>
 
       <form action="submit" name="work-experience2" className="text-center">
-        <label htmlFor="work-experience2" className="text-center">
+        <label htmlFor="work-experience2" className="text-center text-sm">
           Inputfeld für RDP Kontoauzug
         </label>
         <textarea
           name="work-experience2"
           rows="10"
           cols="150"
-          className="bg-slate-300 border-2 rounded text-black p-4"
+          className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-100"
         ></textarea>
         <button
           type="submit"
@@ -62,14 +73,14 @@ function Futterplatz() {
       </form>
 
       <form action="submit" name="work-experience3" className="text-center">
-        <label htmlFor="work-experience3" className="text-center">
+        <label htmlFor="work-experience3" className="text-center text-sm">
           Inputfeld für Inventurliste
         </label>
         <textarea
           name="work-experience3"
           rows="10"
           cols="150"
-          className="bg-slate-300 border-2 rounded text-black p-4"
+          className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-100"
         ></textarea>
         <button
           type="submit"
@@ -78,32 +89,6 @@ function Futterplatz() {
           Rein ins Backend
         </button>
       </form>
-      <fieldset>
-        <legend>Meine Lieblings-Programmiersprache:</legend>
-        <div>
-          <input type="radio" id="css-language" name="language" value="css" />
-          <label htmlFor="css-language">CSS</label>
-        </div>
-        <div>
-          <input
-            type="radio"
-            id="html-language"
-            name="language"
-            value="html"
-            checked
-          />
-          <label htmlFor="html-language">HTML</label>
-        </div>
-        <div>
-          <input
-            type="radio"
-            id="javascript-language"
-            name="language"
-            value="javascript"
-          />
-          <label htmlFor="javascript-language">Javascript</label>
-        </div>
-      </fieldset>
     </div>
   );
 }
