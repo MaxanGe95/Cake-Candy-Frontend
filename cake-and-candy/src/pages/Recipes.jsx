0c4200109fe65e3c39ddeb4b1dc7c367682689e6
Nelly <nelly.fuerst@outlook.de>
2025-03-10 13:26:38 +0100

    commit

--- cake-and-candy/src/pages/Recipes.jsx
+++ cake-and-candy/src/pages/Recipes.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import RecipeForm from "../components/recipe/RecipeForm";
 import RecipeList from "../components/recipe/RecipeList";
+// import RecipeDetails from "../components/recipe/RecipeDetails";
 import { fetchZutaten } from "../api/zutaten";
 import { fetchRezepte, deleteRezept } from "../api/rezepte";
 import { PrimaryButton } from "../components/form/Buttons";
@@ -8,10 +9,11 @@ import { PrimaryButton } from "../components/form/Buttons";
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [showNewRecipeForm, setShowNewRecipeForm] = useState(false);
   const [editMode, setEditMode] = useState(false);
-  const [ingredientsList, setIngredientsList] = useState([]);
+  const [ingredientsList, setIngredientsList] = useState([]); // Zutatenliste hinzufügen
 
-  // Zutaten und Rezepte aus dem Backend laden
+  // Zutaten, Rezepte aus dem Backend laden
   useEffect(() => {
     loadZutaten();
     loadRezepte();
@@ -22,14 +24,14 @@ const Recipes = () => {
       const data = await fetchRezepte();
       setRecipes(data);
     } catch (error) {
-      console.error("Fehler beim Laden der Rezepte:", error);
+      console.error("Fehler beim Laden der Zutaten:", error);
     }
   };
 
   const loadZutaten = async () => {
     try {
       const data = await fetchZutaten();
-      setIngredientsList(data);
+      setIngredientsList(data); // Zutatenliste im State speichern
     } catch (error) {
       console.error("Fehler beim Laden der Zutaten:", error);
     }
@@ -38,47 +40,72 @@ const Recipes = () => {
   const handleSaveRecipe = (recipe) => {
     console.log("Rezept gespeichert:", recipe);
     loadRezepte();
+
     setEditMode(false);
+    setShowNewRecipeForm(false);
     setSelectedRecipe(null);
   };
 
   const handleEditRecipe = (recipe) => {
     setSelectedRecipe(recipe);
+    setShowNewRecipeForm(true);
     setEditMode(true);
   };
 
   const handleDeleteRecipe = async (id) => {
-    await deleteRezept(id);
-    loadRezepte();
-    setSelectedRecipe(null);
+    deleteRezept(id)
+      .then(loadRezepte)
+      .then(() => setSelectedRecipe(null));
   };
 
   const handleCancel = () => {
+    setShowNewRecipeForm(false);
     setSelectedRecipe(null);
     setEditMode(false);
   };
 
   return (
     <div className="p-8 text-amber-100">
-      <RecipeForm
-        recipe={
-          selectedRecipe || {
-            name: "",
-            ingredients: [{ name: "", amount: 0, ekPreis: 0 }],
-            tools: [],
-            totalAmount: 1,
+      {showNewRecipeForm && (
+        <RecipeForm
+          recipe={
+            selectedRecipe || {
+              name: "",
+              ingredients: [{ name: "", amount: 0, ekPreis: 0 }],
+              tools: [],
+              totalAmount: 1,
+            }
           }
-        }
-        onSave={handleSaveRecipe}
-        onCancel={handleCancel}
-        ingredientsList={ingredientsList} // Zutatenliste als Prop an RecipeForm übergeben
-      />
-      <RecipeList
-        recipes={recipes}
-        onSelect={setSelectedRecipe}
-        onDelete={handleDeleteRecipe}
-        onEdit={handleEditRecipe}
-      />
+          onSave={handleSaveRecipe}
+          onCancel={handleCancel}
+          ingredientsList={ingredientsList} // Zutatenliste als Prop an RecipeForm übergeben
+        />
+      )}
+      {!selectedRecipe && !showNewRecipeForm && (
+        <div>
+          <PrimaryButton
+            onClick={() => setShowNewRecipeForm(true)}
+            className="mb-4 mt-10"
+          >
+            Neues Rezept hinzufügen
+          </PrimaryButton>
+          <RecipeList
+            recipes={recipes}
+            onSelect={setSelectedRecipe}
+            onDelete={handleDeleteRecipe}
+            onEdit={handleEditRecipe}
+          />
+        </div>
+      )}
+      {selectedRecipe && !showNewRecipeForm && (
+        <div>
+          <RecipeDetails
+            recipe={selectedRecipe}
+            onEdit={handleEditRecipe}
+            onCancel={handleCancel}
+          />
+        </div>
+      )}
     </div>
   );
 };
