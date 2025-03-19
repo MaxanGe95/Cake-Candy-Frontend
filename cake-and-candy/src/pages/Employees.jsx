787b065d833390a3db4b4bb66fc6377c99274efd
Nelly <nelly.fuerst@outlook.de>
2025-03-19 09:17:58 +0100

    commit

--- cake-and-candy/src/pages/Employees.jsx
+++ cake-and-candy/src/pages/Employees.jsx
@@ -111,7 +111,7 @@ const MitarbeiterTabelle = () => {
     <div className="container mx-auto p-6">
 
        <table className="min-w-full text-amber-100 border border-teal-950 rounded-md overflow-hidden text-center">
-        <thead className="bg-teal-950">
+        <thead className="bg-teal-950 ">
           <tr className="text-white">
             <th className="p-2">Mitarbeiter</th>
             <th className="p-2">Gesamtgehalt</th>
@@ -119,11 +119,11 @@ const MitarbeiterTabelle = () => {
           </tr>
         </thead>
 
-        <tbody>
+        <tbody className="">
           {Object.entries(data).map(([employeeName, employee]) => (
             <React.Fragment key={employeeName}>
               <tr
-                className="cursor-pointer border border-amber-100 text-white"
+                className="cursor-pointer hover:bg-[#7ec6cc80] border border-amber-100 text-white"
                 onClick={() => setSelectedEmployee(selectedEmployee === employeeName ? null : employeeName)}
               >
                 <td className="p-2 text-left">{employeeName}</td>
@@ -139,7 +139,7 @@ const MitarbeiterTabelle = () => {
                 Object.keys(employee.months).map((month) => (
                   <React.Fragment key={month}>
                     <tr
-                      className="cursor-pointer bg-teal-900 text-white"
+                      className="cursor-pointer hover:bg-[#7ec6cc80] bg-teal-900 text-white"
                       onClick={() => setSelectedMonth((prev) => ({
                         ...prev,
                         [employeeName]: prev[employeeName] === month ? null : month,
@@ -157,7 +157,7 @@ const MitarbeiterTabelle = () => {
                       Object.keys(employee.months[month].weeks).map((week) => (
                         <React.Fragment key={week}>
                           <tr
-                            className="cursor-pointer bg-teal-800 text-white border-1 border-amber-300"
+                            className="cursor-pointer hover:bg-[#7ec6cc80] bg-teal-800 shadow-lg text-white border-1 border-amber-300"
                             onClick={() => setSelectedWeek((prev) => ({
                               ...prev,
                               [`${employeeName}-${month}`]: prev[`${employeeName}-${month}`] === week ? null : week,
